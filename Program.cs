2a47e0d7f30d1c1e4187fa53642c5473e0d02da6
Adam <adam.erlandsson94@gmail.com>
2022-01-26 19:21:39 +0100

    Able to check if order is correct

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 namespace XMLConverterApp {
     class XMLConverter {
 
+        static private Dictionary<int, string[]> myDict = new Dictionary<int, string[]>();
         static void ReadTxtFile()
         {
             Console.WriteLine("Trying to read the text file 'rowbased_format.txt'...");
@@ -12,40 +13,90 @@ namespace XMLConverterApp {
             const string path = @"rowbased_format.txt";
             string fileName = Path.GetFileName(path);
         
-            try
-            {
-                string[] lines = File.ReadAllLines(fileName);
-                Dictionary<int, string[]> myDict = new Dictionary<int, string[]>();
-                int dictIndex = 0;
+           
+            string[] lines = File.ReadAllLines(fileName);
+            
+            int dictIndex = 0;
 
-                foreach(string line in lines)
-                {
-                    string[] splittedLine = line.Split('|');
-                    myDict.Add(dictIndex, splittedLine);
-                    dictIndex++;
-                }
-
-                /*foreach(KeyValuePair<int, string[]> ele1 in myDict)
-                {
-                    Console.Write("{0} containts: ",ele1.Key);
-                    foreach(string arr in ele1.Value)
-                    {
-                        Console.Write("{0} ", arr);
-                    }
-                    Console.WriteLine();
-                }*/
-                Console.WriteLine("File successfully read!");
-            }
-            catch (FileNotFoundException e)
+            foreach(string line in lines)
             {
-                Console.WriteLine("{0} Exception caught.", e);
-                System.Environment.Exit(1);
+                string[] splittedLine = line.Split('|');
+                myDict.Add(dictIndex, splittedLine);
+                dictIndex++;
             }
-            //Console.WriteLine("PATH:     {0}", path);
-            //Console.WriteLine("FILENAME: {0}", filename);
+
+            Console.WriteLine("File successfully read!");
 
         }
+
+        static void WriteToXML()
+        {
+            string previousLetter = "";
+            var pLetters = new List<string> {"T", "A", "F"};
+            var fLetters = new List<string> {"T", "A"};
+
+            foreach(KeyValuePair<int, string[]> element in myDict)
+            {
+                if(String.Equals(element.Value[0], "P"))
+                {
+                    if(String.Equals(previousLetter, "F") || String.Equals(previousLetter, "P"))
+                    {
+                        Console.WriteLine("Error! The letter '{0}' cannot be used after the '{1}'.", element.Value[0], previousLetter);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Previous letter is: '{0}'. Current letter is: '{1}'.", previousLetter, element.Value[0]);
+                        previousLetter = "P";
+                    }
+
+                }
+                else if(String.Equals(element.Value[0], "F"))
+                {
+                    if(String.Equals(previousLetter, "F"))
+                    {
+                        Console.WriteLine("Error! The letter '{0}' cannot be used after the '{1}'.", element.Value[0], previousLetter);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Previous letter is: '{0}'. Current letter is: '{1}'.", previousLetter, element.Value[0]);
+                        previousLetter = "F";
+                    }
+                }
+                else
+                {
+                    if(String.Equals(previousLetter, "P") && pLetters.Contains(element.Value[0]))
+                    {
+                        Console.WriteLine("Previous letter is: '{0}'. Current letter is: '{1}'.", previousLetter, element.Value[0]);
+                    }
+                    else if(String.Equals(previousLetter, "F") && fLetters.Contains(element.Value[0]))
+                    {
+                        Console.WriteLine("Previous letter is: '{0}'. Current letter is: '{1}'.", previousLetter, element.Value[0]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error! The order is incorrect.");
+                        Console.WriteLine("Previous letter is: '{0}'. Current letter is: '{1}'.", previousLetter, element.Value[0]);
+
+                        // EXIT?
+                    }
+                    //Console.WriteLine("Should not enter");
+                }
+
+                
+
+                //Console.WriteLine(element.Value[0]);
+
+                // Console.Write("{0} containts: ",ele1.Key);
+                // foreach(string arr in ele1.Value)
+                // {
+                //     Console.Write("{0} ", arr);
+                // }
+                
+            }
+        }
+
         static void Main(string[] args) {
+
             Console.WriteLine("------------------------------------------------------");
             Console.WriteLine("------ Rowbased textfile converter to XML format -----");
             Console.WriteLine("--------------- By Adam Erlandsson -------------------");
@@ -53,7 +104,7 @@ namespace XMLConverterApp {
             Console.WriteLine();
 
             ReadTxtFile();
-        
+            WriteToXML();
         }
     }
 }
